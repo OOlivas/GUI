7d384ec009c15f20ff11b54a7e3f6d58c6264b30
Omar <oolivasp@asu.edu>
2022-03-28 23:26:55 -0700

    Created get/set methods for username, email, and creditCard. Set Customer as subclass of User.

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,32 @@
-public class Customer {
+public class Customer extends User {
+    private String username,
+            email,
+            creditCard;
+
+    /*public void Login();
+    public void createAccount();
+    public void setCustomerInfo(String name, String address, String cardNumber);
+    public bool getCustomerInfo();
+    public bool hasCoupon();*/
+
+    public String getUsername(){
+        return username;
+    }
+    public void setUsername(String newUsername){
+        this.username = newUsername;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String newEmail){
+        this.email = newEmail;
+    }
+
+    public String getCreditCard(){
+        return creditCard;
+    }
+    public void setCreditCard(String newCreditCard){
+        this.creditCard = newCreditCard;
+    }
 }
